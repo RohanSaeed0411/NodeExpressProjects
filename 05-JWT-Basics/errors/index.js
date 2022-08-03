2843a66c22a208a05e6deb3c0f6a952a2ba8b138
RohanSaeed0411 <rohan.saeed.638@gmail.com>
2022-08-03 20:53:29 +0500

    jwt-basics done

---
+++ 05-JWT-Basics/errors/index.js
@@ -0,0 +1,7 @@
+const CustomAPIError = require("./custom-error");
+
+const BadRequestError = require("./bad-request");
+
+const UnauthorizedError = require("./unauthorization");
+
+module.exports = { CustomAPIError, BadRequestError, UnauthorizedError };
